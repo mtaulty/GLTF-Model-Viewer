b3a9efd21cf927906d5c88b0c383ae0c0e32afa8
Mike Taulty <mtaulty@microsoft.com>
2018-11-01 13:01:54 +0000

    Experiment with UnityGLTF submodule over, back to just using toolkit

--- setup.bat
+++ setup.bat
@@ -1,1 +1,5 @@
+msbuild /t:restore UnityGLTF\GLTFSerialization\GLTFSerializationUWP\GLTFSerializationUWP.csproj
+
+msbuild /p:Configuration=Release UnityGLTF\GLTFSerialization\GLTFSerializationUWP\GLTFSerializationUWP.csproj
+
 mklink /j GLTFModelViewer\Assets\HoloToolkit .\MixedRealityToolkit-Unity\Assets\HoloToolkit
