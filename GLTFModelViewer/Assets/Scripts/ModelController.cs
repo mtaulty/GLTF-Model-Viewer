982612802141f2977d88784b64ca0f5033c5d417
Mike Taulty <mtaulty@microsoft.com>
2018-11-02 16:06:04 +0000

    More done with scaling, added a progress indicator which is broken

--- GLTFModelViewer/Assets/Scripts/ModelController.cs
+++ GLTFModelViewer/Assets/Scripts/ModelController.cs
@@ -1,5 +1,6 @@
 ﻿using HoloToolkit.Unity.InputModule.Utilities.Interactions;
 using HoloToolkit.Unity.UX;
+using HoloToolkit.UX.Progress;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityGLTF;
@@ -55,13 +56,19 @@ public class ModelController : AwaitableMonoBehaviour
 
         if (stream != null)
         {
+            ProgressIndicator.Instance.Open(
+                IndicatorStyleEnum.AnimatedOrbs, 
+                ProgressStyleEnum.ProgressBar, 
+                ProgressMessageStyleEnum.Visible, 
+                "Loading...");
+
             // Try to load that model
-            // TODO: what's the root path here?
-            // TODO: set the parent here
             GLTFSceneImporter importer = new GLTFSceneImporter(
                 "/", stream, null, true);
 
             await base.RunCoroutineAsync(importer.Load(-1, true));
+
+            ProgressIndicator.Instance.Close();
 
             // Did we load?
             if (importer.LastLoadedScene != null)
@@ -79,25 +86,19 @@ public class ModelController : AwaitableMonoBehaviour
 
     void AddNewGLTFModel(GameObject loadedModel)
     {
-        // Move the parent to be approx 3m down the user's gaze.
-        var parentPosition =
-            Camera.main.transform.position +
-            Camera.main.transform.forward * MODEL_START_DISTANCE;
-
-        // Patch up the y-value to try and line it up with the head position.
-        parentPosition.y = Camera.main.transform.position.y;
-
-        this.GLTFModelParent.transform.position = parentPosition;
+        this.PositionParentForModel();
 
         // Parent the new model off our parent object.
         loadedModel.transform.SetParent(this.GLTFModelParent.transform, false);
+        loadedModel.transform.localPosition = Vector3.zero;
 
-        // Now need to add behaviours for rotate, transform, scale, etc.
-        var twoHandManips = loadedModel.gameObject.AddComponent<TwoHandManipulatable>();
-        twoHandManips.BoundingBoxPrefab = this.boundingBoxPrefab;
-        twoHandManips.ManipulationMode = ManipulationMode.MoveScaleAndRotate;
-        twoHandManips.RotationConstraint = AxisConstraint.None;
+        this.AddManipulationsToModel(loadedModel);
 
+        this.InitialSizeModel(loadedModel);
+    }
+
+    void InitialSizeModel(GameObject loadedModel)
+    {
         // Need to do something about setting the model to a reasonable size.
         var collider = loadedModel.gameObject.GetComponentInChildren<Collider>();
 
@@ -106,18 +107,44 @@ public class ModelController : AwaitableMonoBehaviour
             // TODO: does querying bounds like this work at this point?
             var bounds = collider.bounds;
 
-            // what's the max bound here?
-            var maxBound = Mathf.Max(bounds.extents.x, bounds.extents.y, bounds.extents.z);
+            // what's the max extent here? 
+            var maxExtent = Mathf.Max(bounds.extents.x, bounds.extents.y, bounds.extents.z);
 
-            // what the scale factor we need then?
-            var scaleFactor = MODEL_START_SIZE / maxBound;
+            // what the scale factor we need then (extent is half the size of the box).
+            var scaleFactor = MODEL_START_SIZE / (maxExtent * 2.0f);
 
             // scale it.
-            loadedModel.gameObject.transform.localScale *= scaleFactor;
+            loadedModel.gameObject.transform.localScale = Vector3.one * scaleFactor;
 
+            // record it so that we can put it back on the 'reset' command.
             this.initialScaleFactor = loadedModel.gameObject.transform.localScale;
         }
     }
+
+    void AddManipulationsToModel(GameObject loadedModel)
+    {
+        // Now need to add behaviours for rotate, transform, scale, etc.
+        var twoHandManips = loadedModel.gameObject.AddComponent<TwoHandManipulatable>();
+        twoHandManips.BoundingBoxPrefab = this.boundingBoxPrefab;
+        twoHandManips.ManipulationMode = ManipulationMode.MoveScaleAndRotate;
+        twoHandManips.RotationConstraint = AxisConstraint.None;
+    }
+
+    void PositionParentForModel()
+    {
+        // Move the parent to be approx 3m down the user's gaze.
+        var parentPosition =
+            Camera.main.transform.position +
+            Camera.main.transform.forward * MODEL_START_DISTANCE;
+
+        // Patch up the y-value to try and line it up with the head position.
+        parentPosition.y = Camera.main.transform.position.y;
+
+        // Move the parent to this new position. From there, the parent doesn't
+        // get moved, scaled, rotated, only the model (child) will.
+        this.GLTFModelParent.transform.position = parentPosition;
+    }
+
     GameObject CurrentModel
     {
         get
