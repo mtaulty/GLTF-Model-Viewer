0b310847ddc5045c03f27ac74a43f2de40636e90
Mike Taulty <mtaulty@microsoft.com>
2018-11-29 18:36:41 +0000

    Added web server and tested that it serves up files from the 3D Objects folder & sub folders & decent error codes for not-found files

---
+++ GLTFModelViewer/Assets/Scripts/MonoBehaviours/WebServerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+#if ENABLE_WINMD_SUPPORT
+using UwpHttpServer;
+using Windows.Storage;
+#endif 
+
+public class WebServerManager : MonoBehaviour
+{
+    [SerializeField]
+    int listenPort = 8088;
+
+#if ENABLE_WINMD_SUPPORT
+
+    async void Start()
+    {
+        this.webServer = new StorageFolderWebServer(
+            KnownFolders.Objects3D,
+            this.listenPort);
+
+        await this.webServer.RunAsync(null);
+    }
+    StorageFolderWebServer webServer;
+#endif // ENABLE_WINMD_SUPPORT
+}
---
+++ GLTFModelViewer/Assets/Scripts/WebServer/HttpListenerContextExtensions.cs
@@ -0,0 +1,24 @@
+﻿#if ENABLE_WINMD_SUPPORT
+
+using System.Linq;
+using System.Net;
+
+namespace UwpHttpServer
+{
+    internal static class HttpListenerContextExtensions
+    {
+        public static bool VerbIsGet(this HttpListenerContext context) =>
+            (string.Compare(context?.Request?.HttpMethod, "get", true) == 0);
+
+        public static bool AcceptsBinary(this HttpListenerContext context)
+        {
+            var valuesWithoutQuality = context.Request.AcceptTypes.Select(
+                acceptType => acceptType.Split(';').First());
+
+            return (
+                valuesWithoutQuality.Contains("application/octet-stream") ||
+                valuesWithoutQuality.Contains("*/*"));
+        }
+    }
+}
+#endif // ENABLE_WINMD_SUPPORT
