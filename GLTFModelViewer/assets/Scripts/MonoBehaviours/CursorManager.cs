ac03cead5239e1606bd6a378913c07bcaf01de0b
mtaulty <mtaulty@hotmail.com>
2019-07-02 14:22:33 +0100

    Modified cursor show/hide to try to account for new toolkit

--- GLTFModelViewer/assets/Scripts/MonoBehaviours/CursorManager.cs
+++ GLTFModelViewer/assets/Scripts/MonoBehaviours/CursorManager.cs
@@ -1,17 +1,41 @@
-﻿using HoloToolkit.Unity.InputModule;
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField]
-    private ObjectCursor cursor;
-
-    public void Show()
+    public CursorManager()
     {
-        //this.cursor.gameObject.SetActive(true);
+        this.hiddenPointers = new List<IMixedRealityPointer>();
     }
     public void Hide()
     {
-        //this.cursor.gameObject.SetActive(false);
+        // TODO: I need to understand how you are supposed to do this on V2, I just want
+        // to switch all cursors off when the user cannot do anything useful with them.
+        foreach (var inputSource in MixedRealityToolkit.InputSystem.DetectedInputSources)
+        {
+            foreach (var pointer in inputSource.Pointers)
+            {
+                if ((pointer.IsActive) && (pointer.BaseCursor != null))
+                {
+                    pointer.BaseCursor.SetVisibility(false);
+                    this.hiddenPointers.Add(pointer);
+                }
+            }
+        }
+        MixedRealityToolkit.InputSystem.GazeProvider.Enabled = false;
     }
+    public void Show()
+    {
+        foreach (var pointer in this.hiddenPointers)
+        {
+            pointer.BaseCursor.SetVisibility(true);
+        }
+        this.hiddenPointers.Clear();
+
+        MixedRealityToolkit.InputSystem.GazeProvider.Enabled = true;
+    }
+    List<IMixedRealityPointer> hiddenPointers;
 }
